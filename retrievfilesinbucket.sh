64db377071379968bcb8036907fe33c666ebcfe2
Laurent Domb <laurent@domb.com>
2014-04-22 22:25:35 -0400

    added nice colors for md5 sum

--- retrievfilesinbucket.sh
+++ retrievfilesinbucket.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-# intial script written by Lian Sutton
-# https://groups.google.com/forum/#!topic/puppet-users/HenpMKLDZ4k
+green="$(tput setaf 2)"
+red="$(tput setaf 1)"
+reset="$(tput sgr0)"
 
 if [ ${#@} == 0 ]; then
-    echo "#################################################################"
+    echo "${green}#################################################################"
     echo "# This script is used to retrieve backups of files that were    #"
     echo "# updated by puppet but which you might want to retrieve or     #"
     echo "# reinstate. Remember that unless you add the retrieved file    #"
@@ -11,29 +11,31 @@ if [ ${#@} == 0 ]; then
     echo "#                                                               #"
     echo "# Usage: `basename $0` file_to_look_for               #" 
     echo "# Example: `basename $0` /                            #"
-    echo "#################################################################"
+    echo "################################################################# ${reset}"
 else
     file=$1
     BUCKETDIR=$( puppet config print clientbucketdir )
     PREFIX="* "
     entries_found=0
     echo "Searching for local backups of file $file (this may take some time)..."
+    echo "====================================================================================="
     for f in $( find $BUCKETDIR -type f -name paths -exec grep -l $file {} \; |xargs -r ls -t);
     do
       md5sum=$( basename $(dirname $f))
       filename=$(< $f )
       modify_time=$(stat --format '%y' $f)
-      echo -e "$filename\t$md5sum\t$modify_time"
+      echo -e "$filename\t${red}$md5sum\t${reset}$modify_time"
       entries_found=1
     done
   if [ $entries_found -eq 1 ]; then
     echo ""
-    echo "Backup(s) were found!"
-    echo "* To view the contents of the file, issue this command: "
-    echo "puppet filebucket -b $BUCKETDIR get <md5sum>"
-    echo " "
-    echo "* To restore the file, issue this command: "
-    echo "puppet filebucket restore -b $BUCKETDIR /path/to/new/file <md5sum>"
+    echo "${green}Backup(s) were found!${reset}                     "
+    echo " * To view the contents of the file, issue this command:  "
+    echo " puppet filebucket -b $BUCKETDIR get ${red}<md5sum>${reset}"
+    echo "                                                          "
+    echo " * To restore the file, issue this command:               "
+    echo " puppet filebucket restore -b $BUCKETDIR /path/to/new/file ${red}<md5sum>${reset}"
+    echo "====================================================================================="
   else
     echo "No entries were found. Exiting."
   fi
